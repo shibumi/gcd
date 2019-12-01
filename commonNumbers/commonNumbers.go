b8607351b2d03df3fe25c02c99b303b0f8612c83
Christian Rebischke <chris@nullday.de>
2019-12-01 22:37:17 +0100

    add commonNumbers.go
    This checks for common numbers in two int slices

---
+++ commonNumbers/commonNumbers.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type REQUEST struct {
+	X      []int `json:"x" binding:"required"`
+	Y      []int `json:"y" binding:"required"`
+	RESULT []int `json:"number"`
+}
+
+func main() {
+	router := gin.Default()
+	v1 := router.Group("/api/v1/")
+	{
+		v1.POST("/", commonNumbers)
+	}
+	router.Run()
+}
+
+// commonNumbers checks for common numbers in two int slices.
+func commonNumbers(context *gin.Context) {
+	var req REQUEST
+	if err := context.BindJSON(&req); err != nil {
+		context.AbortWithStatusJSON(http.StatusBadRequest, err)
+		return
+	}
+	for _, xe := range req.X {
+		for _, ye := range req.Y {
+			if xe == ye {
+				req.RESULT = append(req.RESULT, xe)
+			}
+		}
+	}
+	context.AbortWithStatusJSON(http.StatusOK, req)
+	return
+}
