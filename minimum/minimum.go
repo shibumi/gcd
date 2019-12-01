99ccd188fc9b90425e4f281d3cb5dc77d8c12ba9
Christian Rebischke <chris@nullday.de>
2019-12-02 00:19:36 +0100

    add minimum.go

---
+++ minimum/minimum.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type REQUEST struct {
+	X      []int `json:"x" binding:"required"`
+	RESULT int   `json:"result"`
+}
+
+func main() {
+	router := gin.Default()
+	v1 := router.Group("/api/v1/")
+	{
+		v1.POST("/", minimum)
+	}
+	router.Run()
+}
+
+func minimum(context *gin.Context) {
+	var req REQUEST
+	if err := context.BindJSON(&req); err != nil {
+		context.AbortWithError(http.StatusBadRequest, err)
+	}
+	for i, e := range req.X {
+		if i == 0 || e < req.RESULT {
+			req.RESULT = e
+		}
+	}
+	context.AbortWithStatusJSON(http.StatusOK, req)
+	return
+}
